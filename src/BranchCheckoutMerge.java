c709d6993c47a94fc938e60d072d5ff6fd1df1c2
HariharanUK <hariharandhanpaluk@gmail.com>
2019-05-28 04:31:00 +0530

    testing this branch of new-feature appear in github site

--- src/BranchCheckoutMerge.java
+++ src/BranchCheckoutMerge.java
@@ -4,6 +4,7 @@ public class BranchCheckoutMerge {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Gitcha");
+		System.out.println("Gitcha updating for branch checkout merge");
 	}
 
 }
